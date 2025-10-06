aae9b8f2f8c6fb740e9e69584218ccf86b9b4f9b
Hasha <daniel.trandafir2005@gmail.com>
2025-10-06 19:48:13 +0200

    madman invents touhou

--- Touhou Sunset Kaleidoscope/Assets/Scripts/Main Game/Scriptable Objects/Bullets/Enemy/Enemy Shot Pattern.cs
+++ Touhou Sunset Kaleidoscope/Assets/Scripts/Main Game/Scriptable Objects/Bullets/Enemy/Enemy Shot Pattern.cs
@@ -6,7 +6,7 @@ namespace KH
         [Header("Enemy Shot Pattern Data")]
         public GameObject bulletPrefab;
         public Sprite bulletSprite;
-        public float defaultBulletSpeed = 500f;
+        public float defaultBulletSpeed = 3f;
 
         // Virtual enables the method to be overridden by child classes
         public virtual void Fire(Vector2 origin)
